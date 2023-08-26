d486608b1bceadec6a5c0b6b4733d4a02631fd14
TrongToan <toancccccccc@gmail.com>
2023-08-26 11:10:28 +0700

    updatecodenew

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, clipboard } = require('electron');
+const { app, Menu, MenuItem, BrowserWindow, clipboard } = require('electron');
 const path = require('path');
 const server = require('./index');
 function createWindow() {
@@ -11,33 +11,24 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+  const template = [
+    { label: 'Dán', role: 'paste' },
+    { label: 'Sao Chép', role: 'copy' },
+    { label: 'Cắt', role: 'cut' },
+  ];
+
+  const editMenu = Menu.buildFromTemplate(template);
 
   win.loadFile('index.html');
 
+  win.webContents.on('context-menu', (e, props) => {
+    console.log(props.x, props.y);
+    editMenu.popup(win, props.x, props.y);
+  });
   // Khởi chạy child process để chạy server Express
   server.listen(3000, () => {
     console.log('Server is on port 3000 !!!');
   });
-  // Mở DevTools cho cửa sổ chính
-  // win.webContents.openDevTools();
-
-  // // Lắng nghe sự kiện context menu
-  // win.webContents.on('context-menu', (e, params) => {
-  //   const { x, y } = params;
-  //   const copiedData = clipboard.readText();
-  //   console.log(copiedData);
-
-  //   // Kiểm tra xem có dữ liệu trong clipboard hay không
-  //   if (copiedData) {
-  //     // Đặt dữ liệu vào thẻ input hoặc vị trí mong muốn
-  //     win.webContents.executeJavaScript(
-  //       `
-  //       document.querySelector('.name__input').value = "${copiedData}";
-  //     `,
-  //       true
-  //     );
-  //   }
-  // });
 }
 
 app.whenReady().then(() => {
